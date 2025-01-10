551b2e646fe5dffa754342e2c091c680aaf34123
av1401 <119588311+av1401@users.noreply.github.com>
2025-01-10 10:54:21 +0530

    Delete Day01 directory

--- Day01/Level_1_ProgrammingElementsPractices/FindAge.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class FindAge{
-	public static void main(String args[]){
-		int yearOfBirth = 2000;
-		int currentAge = 2024 - yearOfBirth;
-		// outputs the age of harry using his year of birth
-		System.out.println("Harry's age in 2024 is: " + currentAge);
-	}
-}
--- Day01/Level_1_ProgrammingElementsPractices/KmToMiles.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class KmToMiles{
-    public static void main(String[] args) {
-        double kilometers = 10.8;
-        double miles = kilometers * 0.6; 
-	// 1 km = 0.6 miles
-        System.out.print("The distance " +kilometers + " km in miles is " + miles);
-    }
-}
